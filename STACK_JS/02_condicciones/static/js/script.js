c9cada77e174d0ea9c8d24f5ad036c2ae27952a2
j-aaviieer <266820266+j-aaviieer@users.noreply.github.com>
2026-05-25 16:07:30 -0400

    asd

---
+++ STACK_JS/02_condicciones/static/js/script.js
@@ -0,0 +1,11 @@
+console.log(`conexion exitosa con js...`)
+
+function sistemaDescuento(){
+    let nombre = prompt("Ingrese su nombre:");
+    let monto = parseInt(prompt("Ingrese el monto de compra:"));
+    if (monto >= 50000){
+        alert(`El monto con descuento es: ${monto - (monto = 0.2)}`);{
+    } else if (monto >= 30000){
+        
+    }
+}
