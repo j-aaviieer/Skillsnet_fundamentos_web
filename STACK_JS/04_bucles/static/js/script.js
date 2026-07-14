af301efac980462b7aec44f84de9a881e1908ef4
j-aaviieer <266820266+j-aaviieer@users.noreply.github.com>
2026-07-14 14:46:48 -0400

    asd

---
+++ STACK_JS/04_bucles/static/js/script.js
@@ -0,0 +1,11 @@
+function contarHasta2() {
+    for (let i = 0; i < 3; i++) {
+        alert(i);
+    }
+}
+
+function cuentaRegresiva() {
+    for (let i = 10; i > 0; i--) {
+        alert(i);
+    }
+}
