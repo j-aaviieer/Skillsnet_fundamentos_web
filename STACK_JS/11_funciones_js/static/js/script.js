c666f73b80ccae95fa9c2387352a58d20366ba6e
j-aaviieer <javiercolque@liceovvh.cl>
2026-08-10 08:15:46 -0400

    asd

--- STACK_JS/11_funciones_js/static/js/script.js
+++ STACK_JS/11_funciones_js/static/js/script.js
@@ -35,14 +35,32 @@ function encontrarMayor() {
 /* Crear una funcion que reciba 3 parametros, a, b y c.
 Debe sumar a + b yel resultado restarlo por c.
 Devolver el valor final y mostrar con un alert. */
-function calcularOperacion() {
-   function calcular(a, b, c) {
-      let resultado = (a + b) - c;
-   }
-   let numero1 = parseInt(prompt("ingrese el numero 1"));
-   let numero2 = parseInt(prompt("Ingrese el numero 2"));
-   let numero3 = parseInt(prompt("Ingrese el numero 3"));
-   let resultado = calcular(numero1, numero2, numero3);
-   alert(`El resultaro de ${numero1}, ${numero2} menos ${numero3} es: ${resultado}`);
-   return resultado;
+
+//Tarea resuelta
+
+function operaciones(a, b, c) {
+   return a + b - c;
 }
+function mostrarResultado() {
+   let num1 = parseInt(prompt("Ingrese el primer numero"));
+   let num2 = parseInt(prompt("Ingrese el segundo numero"));
+   let num3 = parseInt(prompt("Ingrese el tercer numero"));
+   let resultado = operaciones(num1, num2, num3)
+   alert(`La operacion de ${num1} + ${num2} - ${num3} = ${resultado}`)
+}
+
+/*
+crear una funcion que reciba un parametro y permita a traves de un
+bucle contar hasta este.
+Ej: Se recibe el número 5 y muestra: 1 - 2 - 3 - 4 - 5
+Ingresar una condicion para que el numero agregado no supere los 100
+*/
+
+function contarNumeros() {
+   let fin = parseInt(prompt("Ingrese un número"));
+   for (let i = 1; i <= fin; i++) {
+      numero.push(i);
+      let contar = numero.join(" - ");
+      alert(contar);
+   } 
+   }
