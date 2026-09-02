e7738279bdf8d20bc856504e474f31ef87d84814
j-aaviieer <javiercolque@liceovvh.cl>
2026-09-02 09:19:10 -0400

    asd

--- STACK_JS/18_DOM_core/static/js/script.js
+++ STACK_JS/18_DOM_core/static/js/script.js
@@ -1,23 +1,31 @@
-let boton = document.getElementById("iniciarSesion");
-
-// Agregamos un evento 'click' al botón
-boton.addEventListener("click", function () {
-    let textoBoton = boton.textContent;
-    if(textoBoton == "Iniciar sesión"){
-        // Cambia el texto del botón usando `this`
-        this.innerText = "Cerrar sesión";
-    } else {
-        this.innerText = "Iniciar sesión";
-    }
-});
-
 function verPerfil() {
     alert(`¡Bienvenido al Perfil!`)
 }
 
-let meGusta = document.querySelector(".likes");
-let contador = 0;
-meGusta.addEventListener("click", function () {
-    contador++;
-    this.textContent = `${contador} Me gusta`;
-});
+var likeRei = 0;
+var likeAsuka = 0;
+var likeShinji = 0;
+
+var textoLikesRei = document.getElementById("reiLikes");
+var botonLikeRei = document.getElementById("btnRei");
+
+botonLikeRei.onclick = function () {
+    likeRei = likeRei + 1;
+    textoLikesRei.innerText = likeRei + " like(s)";
+};
+
+var textoLikesAsuka = document.getElementById("asukaLikes");
+var botonLikeAsuka = document.getElementById("btnAsuka");
+
+botonLikeAsuka.onclick = function () {
+    likeAsuka = likeAsuka + 1;
+    textoLikesAsuka.innerText = likeAsuka + " like(s)";
+};
+
+var textoLikesShinji = document.getElementById("shinjiLikes");
+var botonLikeShinji = document.getElementById("btnShinji");
+
+botonLikeShinji.onclick = function () {
+    likeShinji = likeShinji + 1;
+    textoLikesShinji.innerText = likeShinji + " like(s)";
+};
